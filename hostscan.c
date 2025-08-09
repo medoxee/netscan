6794b640a94f0386daa694d430b7086f6739ab3c
medoxee <medoxee99@gmail.com>
2025-08-09 12:10:00 +0100

    Enhanced error handling

--- hostscan.c
+++ hostscan.c
@@ -8,8 +8,8 @@
 #define MAX_IP_LEN 16
 
 /*
- * IN THE NAME OF ALLAH,
- * Date: 12-07-2024
+ *                              IN THE NAME OF ALLAH,
+ * Date: 12-07-2025
  * Developer: MOHAMED ARRAF (https://www.linkedin.com/in/mohamedarraf)
  * Description: Scan for available nodes on the network.
  * Please note that this program is still under development, for now, it is based on OS level
@@ -23,27 +23,27 @@
 
 void	ping(char	hosts_addr[][MAX_IP_LEN], char	online_hosts[][MAX_IP_LEN])
 {
+        /*
+	 * system() returns a value of the termination status of child shell used to
+	 * execute command, e.g: value=0 => ping got a positive echo reply from peer.
+	 * info:
+	 *  man system
+	 *  man ping
+	 *  search on the internet for ping exit status or test it yourself.
+	 */
 	int	exit_status;
 	char	command[50];
 
-	/*
-	 * system() returns a value of the termination status of child shell
-	 * used to execute command, e.g: value=0 => ping got a positive echo reply
-	 * from peer.
-	 * info: man system
-	 * 	 man ping
-	 * 	 search on the internet for ping exit codes or test it yourself.
-	 */
+	printf("scanning for online hosts. Estimated time: 30s\n");
 	for (int i = 0; i < MAX_HOSTS; i++)
 	{
-		snprintf(command, sizeof(command), "ping -c1 -nq -W %s %s", RESPONSE_TIMEOUT, hosts_addr[i]);
+		snprintf(command, sizeof(command), "ping -c1 -nq -W %s %s > /dev/null", RESPONSE_TIMEOUT, hosts_addr[i]);
 		exit_status = system(command);
-		system(CLRSCR);
 		if (exit_status == 0)
 		{
 			strcpy(online_hosts[i], hosts_addr[i]);
 		}
-		else if (exit_status != 0)
+		else
 		{
 			// 0 = host is down, useful when printing online ones
 			online_hosts[i][0] = 0;
@@ -59,23 +59,23 @@ void	generate_ips(char	hosts_addr[][MAX_IP_LEN], char*	network_portion)
 	host_portion = 1;
 	for (int i = 0; i < MAX_HOSTS; i++)
 	{
-		snprintf(hosts_addr[i], sizeof(hosts_addr[MAX_HOSTS]), "%s%d", network_portion, host_portion);
+		snprintf(hosts_addr[i], MAX_IP_LEN, "%s%d", network_portion, host_portion);
 		host_portion++;
 	}
 }
 
 void	get_net_portion(char*	net_addr)
 {
+        /*
+	 * n_dots = number of dots in net_addr, useful to split host and network portions.
+	 * loop below scans for dots in network address,
+	 * when n_dots == 3, replace the next item(192.168.1.0: in this case 0) with NUL
+	 */
 	int	n_dots;
 	int     i;
 
 	i = 0;
 	n_dots = 0;
-	/*
-	 * n_dots = number of dots in net_addr, useful to split host and network portions.
-	 * loop below scans for dots in network address,
-	 * when n_dots == 3, replace the next item(192.168.1.0: in this case 0) with NUL
-	 */
 	while (net_addr[i])
 	{
 		if (net_addr[i] == '.')
@@ -96,13 +96,13 @@ int	main(int	argc, char**	argv)
 	char	hosts_addr[MAX_HOSTS][MAX_IP_LEN];
 	char	online_hosts[MAX_HOSTS][MAX_IP_LEN];
 
-	if (argc == 2)
+	if (argc == 2 && strlen(argv[1]) >= 7)
 	{
 		strcpy(net_addr, argv[1]);
 	}
 	else
 	{
-		printf("Error: bad or missing argument\n");
+		printf("Error: bad or missing argument!\n");
 		return 1;
 	}
 	get_net_portion(net_addr);
@@ -115,7 +115,7 @@ int	main(int	argc, char**	argv)
 	{
 		if (online_hosts[i][0])
 		{
-			printf("  %s\n", online_hosts[i]);
+			printf("%s\n", online_hosts[i]);
 		}
 	}
 	return 0;
